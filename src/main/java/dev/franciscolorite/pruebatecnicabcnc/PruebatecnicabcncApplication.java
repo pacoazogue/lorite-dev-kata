35cf1dba65cc62c428793bd43cd2f76b545a2663
Francisco José Lorite Fernández <pacolori@gmail.com>
2024-01-19 09:37:05 +0100

    Commit inicial - Estructura base del proyecto
    Esqueleto del proyecto tras su generación a través de la herramienta "Spring Initializr"

---
+++ src/main/java/dev/franciscolorite/pruebatecnicabcnc/PruebatecnicabcncApplication.java
@@ -0,0 +1,13 @@
+package dev.franciscolorite.pruebatecnicabcnc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebatecnicabcncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebatecnicabcncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/franciscolorite/pruebatecnicabcnc/PruebatecnicabcncApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.franciscolorite.pruebatecnicabcnc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebatecnicabcncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
